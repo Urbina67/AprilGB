d7fc24b414af0091b55f9a3f42441a1612679fcd
Inga <sonbel@yandex.ru>
2022-04-25 19:38:50 +0300

    New branch for task4

---
+++ Tasks4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу замену элементов массива на противоположные
+int[] Inga=new int[12];
+void FullArray(int[] Maks)
+{
+    int index=0;
+    int lenght=Maks.Length;
+
+    while(index<lenght)
+    {
+        Maks[index]=new Random().Next(0,10);
+        index++;
+    }
+
+}
+void PrintArray(int[] Maks)
+{
+    int count=0;
+    int lenght=Maks.Length;
+    
+    while(count<lenght)
+    {
+       Console.WriteLine($"Элементы массива {-Maks[count]}");
+       count++;
+    }
+
+}
+FullArray(Inga);
+PrintArray(Inga);
