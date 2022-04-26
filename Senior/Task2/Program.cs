6ebc59e4cee9ce2019320506ad4e582814c01c4c
Urbina67 <sonbe@yandex.ru>
2022-04-26 14:57:37 +0300

    Task2

---
+++ Senior/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать массив из 8 элементов, 
+//заполненных нулями и единицами вывести их на экран
+
+void FillArray (int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Array[index] = new Random().Next(0, 2);
+        index++;
+    } 
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(array[position]);
+        position++;
+        
+    }
+}
+int[] Array = new int[8];
+FillArray(Array);
+PrintArray(Array);
