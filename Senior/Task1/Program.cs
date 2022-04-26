1b7a9006ee486251258d59cdb45c50c89fc1aec6
Urbina67 <sonbe@yandex.ru>
2022-04-26 10:12:21 +0300

    Senior 1

---
+++ Senior/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+int[] Sheb = new int[8];
+void Tararam(int[] Ququ)
+{
+    int index=0;
+    int length=Ququ.Length;
+    while (index<length)
+    {
+        Ququ[index]=new Random().Next(0,9);
+        index++;
+    }
+}
+
+void OnPrint(int[] Ququ)
+{
+    int count=0;
+    int length=Ququ.Length;
+    
+    while(count<length)
+    {
+       Console.WriteLine($"{count} элемент массива {Ququ[count]}");
+       count++;
+    }
+
+}
+
+Tararam(Sheb);
+OnPrint (Sheb);
